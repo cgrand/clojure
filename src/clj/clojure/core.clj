417156559046e549ae8a4e2c27cdc0425c55cab9
Christophe Grand <christophe@cgrand.net>
2009-12-23 20:55:37 +0100

    change docstring for delay to specifiy that deref forces. See #230

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -515,9 +515,8 @@
 
 (defmacro delay
   "Takes a body of expressions and yields a Delay object that will
-  invoke the body only the first time it is forced (with force), and
-  will cache the result and return it on all subsequent force
-  calls."  
+  invoke the body only the first time it is forced (with force or deref/@), 
+  and will cache the result and return it on all subsequent force calls."  
   [& body]
     (list 'new 'clojure.lang.Delay (list* `#^{:once true} fn* [] body)))
 
